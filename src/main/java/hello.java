a2839fbe7115dfeda42af0afb7aae80a48cc9323
CTY <1033261860@qq.com>
2022-02-24 22:14:10 +0800

    git_Test第一此commit

---
+++ src/main/java/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author cty
+ * @create 2022-02-24 22:12
+ */
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111111");
+    }
+}
